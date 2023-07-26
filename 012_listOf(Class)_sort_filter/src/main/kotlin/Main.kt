e3cb724c234935858202d2057fcd64b33a65a0d6
peng <songpeng24@msn.com>
2023-07-26 15:47:14 +0800

    high order functions

--- 012_listOf(Class)_sort_filter/src/main/kotlin/Main.kt
+++ 012_listOf(Class)_sort_filter/src/main/kotlin/Main.kt
@@ -17,6 +17,7 @@ fun main(args: Array<String>) {
     val lowestQuantity = groceries.minBy { it.quantity }
     println("lowestQuantity: $lowestQuantity")
 
+    println("\nsumBy:")
     val sumQuantity = groceries.sumBy { it.quantity }
     println("sumQuantity: $sumQuantity")
 
@@ -24,12 +25,14 @@ fun main(args: Array<String>) {
     println("totalPrice: $totalPrice")
 
     //filter
+    println("\nfilter:")
     val vegetables = groceries.filter { it.category == "Vegetable" }
     println("vegetables: $vegetables")
     val notFrozen = groceries.filterNot { it.category == "Frozen" }
     println("notFrozen: $notFrozen")
 
     //create a list with same elements
+    println("\nmap:")
     val groceryNames = groceries.map { it.name }
     println("groceryNames: $groceryNames")
     //reate a List of each unitPrice multiplied by 0.5 using the following code
@@ -55,19 +58,25 @@ fun main(args: Array<String>) {
     println("itemNames: $itemNames")
 
     //groupBy
+    println("\ngroupBy:")
     groceries.groupBy { it.category }.forEach {
-        println(it.key)
+        println("${it.key}:")
         it.value.forEach { println(" ${it.name}") }
     }
 
-    //fold
+    //fold - get sum of each element
+    println("\nfold:")
     val ints = listOf(1, 2, 3)
+    //0 -> add sum start from 0
     val sumOfInts = ints.fold(0) { runningSum, item -> runningSum + item }
     println("sumOfInts: $sumOfInts")
-    val productOfInts = ints.fold(1) { runningProduct, item -> runningProduct * item }
+    //10 -> multi based on 10
+    val productOfInts = ints.fold(10) { runningProduct, item -> runningProduct * item }
     println("productOfInts: $productOfInts")
+    //string fold
     val names = groceries.fold("") { string, item -> string + " ${item.name}" }
     println("names: $names")
+    //double fold
     val changeFrom50 = groceries.fold(50.0) { change, item
         -> change - item.unitPrice * item.quantity }
     println("changeFrom50: $changeFrom50")
