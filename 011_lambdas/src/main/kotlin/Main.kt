1f0cee5790abac60ba650601bcfe3ba19fa10079
peng <songpeng24@msn.com>
2023-07-27 15:18:36 +0800

    inline crossinline

--- 011_lambdas/src/main/kotlin/Main.kt
+++ 011_lambdas/src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 typealias DoubleConversion = (Double) -> Double
 
 //function accept lambdas as parameter
-fun convert(x: Double,
+inline fun convert(x: Double,
             converter: (Double) -> Double) : Double {
     val result = converter(x)
     println("$x is converted to $result")
@@ -9,14 +9,14 @@ fun convert(x: Double,
 }
 
 //function accept lambdas as parameter
-fun convertFive(converter: (Int) -> Double) : Double {
+inline fun convertFive(converter: (Int) -> Double) : Double {
     val result = converter(5)
     println("5 is converted to $result")
     return result
 }
 
 //function return lambdas
-fun getConversionLambda(str: String): DoubleConversion {
+inline fun getConversionLambda(str: String): DoubleConversion {
     if (str == "CentigradeToFahrenheit") {
         return { it * 1.8 + 32 }
     } else if (str == "KgsToPounds") {
@@ -27,8 +27,9 @@ fun getConversionLambda(str: String): DoubleConversion {
         return { it }
     }
 }
-fun combine(lambda1: DoubleConversion,
-            lambda2: DoubleConversion): DoubleConversion {
+//crossinline do not use non-local return which is provided by inline function
+inline fun combine(crossinline lambda1: DoubleConversion,
+                   crossinline lambda2: DoubleConversion): DoubleConversion {
     return { x: Double -> lambda2(lambda1(x)) }
 }
 
