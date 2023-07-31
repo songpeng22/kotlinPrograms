d91a22d96b7db95249830c75bb1aeb03312563fc
peng <songpeng24@msn.com>
2023-07-31 10:02:13 +0800

    when

--- 001_helloKotlin/src/main/kotlin/Main.kt
+++ 001_helloKotlin/src/main/kotlin/Main.kt
@@ -1,5 +1,17 @@
 fun main(args: Array<String>) {
+    //define variable
     var x = 1
+    //print variable
     println("hello kotlin in IntelliJ IDEA.")
     println("x = $x.")
+
+    //when
+    var alignmentIn:Int = 1
+    var alignment:String = when(alignmentIn){
+        1 -> "left"
+        2 -> "center"
+        4 -> "right"
+        else -> "null"
+    }
+    println("alignment:${alignment}")
 }
